1d74cd2f58cd25bfbc955d4408e19eebd5bc06ba
Yecgaa1 <xutongxin@users.noreply.github.com>
2019-04-21 22:14:38 +0800

    json test

--- Python/Mainexe/web/jsontest.py
+++ Python/Mainexe/web/jsontest.py
@@ -3,3 +3,7 @@ import json
 new_dict={'bigberg': [7600, {1: [['iPhone', 6300], ['Bike', 800], ['shirt', 300]]}]}
 with open("./config/record.json","w") as f:
    json.dump(new_dict,f)
+
+with open("./config/record.json",'r') as load_f:
+  load_dict = json.load(load_f)
+  print(load_dict)
