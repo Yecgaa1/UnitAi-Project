fa19729b1a71eca0366bd83da6fad4de442d1396
Yecgaa1 <38446391+Yecgaa1@users.noreply.github.com>
2019-08-10 21:00:01 +0800

    123

--- client/原始图形界面文件/chooseserver.py
+++ client/原始图形界面文件/chooseserver.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'chooseserver.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import  QMainWindow,QApplication
+import sys
 
-class Ui_Dialog(object):
+class Ui_Dialog(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(400, 300)
@@ -16,13 +18,13 @@ class Ui_Dialog(object):
         self.ip.setGeometry(QtCore.QRect(100, 50, 201, 31))
         self.ip.setObjectName("ip")
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(40, 60, 71, 20))
+        self.label.setGeometry(QtCore.QRect(10, 60, 81, 20))
         self.label.setObjectName("label")
         self.port = QtWidgets.QLineEdit(Dialog)
         self.port.setGeometry(QtCore.QRect(100, 110, 201, 31))
         self.port.setObjectName("port")
         self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(70, 120, 54, 12))
+        self.label_2.setGeometry(QtCore.QRect(40, 120, 54, 12))
         self.label_2.setObjectName("label_2")
         self.save = QtWidgets.QPushButton(Dialog)
         self.save.setGeometry(QtCore.QRect(320, 250, 75, 23))
@@ -36,7 +38,7 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
-
+        self.show()
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
@@ -46,4 +48,13 @@ class Ui_Dialog(object):
         self.comboBox.setItemText(0, _translate("Dialog", "无"))
         self.comboBox.setItemText(1, _translate("Dialog", "共享工作站"))
         self.comboBox.setItemText(2, _translate("Dialog", "私有工作站"))
+
+
+if __name__ == '__main__':
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+    Imapp = QApplication(sys.argv)
+    ex = Ui_Dialog()
+    #ex.show()
+    sys.exit(Imapp.exec_())
+
 
