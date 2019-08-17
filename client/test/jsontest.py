dff90802d74e2ae7d53dd5e15dd987dc2da74467
Yecgaa1 <38446391+Yecgaa1@users.noreply.github.com>
2019-08-17 20:55:22 +0800

    123

--- client/test/jsontest.py
+++
@@ -1,9 +0,0 @@
-import json
-
-new_dict={'123': 123}
-with open("./config/record.json","w") as f:
-   json.dump(new_dict,f)
-
-with open("./config/record.json",'r') as load_f:
-  load_dict = json.load(load_f)
-  print(load_dict)
--- client/test/proxy.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMenu,QLineEdit
-from PyQt5.QtCore import QCoreApplication,QTimer,QThread,pyqtSignal
-import sys
-
-def proxy():
-    proxyapp = QApplication(sys.argv)
-    ex = Ui()
-    sys.exit(proxyapp.exec_())
