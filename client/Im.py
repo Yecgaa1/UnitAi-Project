cbc9acb04e7322c7916d05ea083227fca3b0ed35
Yecgaa1 <38446391+Yecgaa1@users.noreply.github.com>
2019-09-22 08:55:06 +0800

    123

--- client/Im.py
+++ client/Im.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore, QtGui, QtWidgets,Qt
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import  QApplication, QPushButton, QMenu,QLineEdit,QMainWindow,QLabel
 from PyQt5.QtCore import QCoreApplication,QTimer,QThread,pyqtSignal
 from PyQt5.QtGui import QIcon, QPainter, QPixmap,QPalette,QBrush
